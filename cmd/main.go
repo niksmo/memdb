937d445ade8b6c8f74cb87d81eaf5a6f7c3dba48
nikolay <n.smolov@yandex.ru>
2026-03-31 22:53:34 +0300

    feat(core): add logger pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/niksmo/memdb/internal"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	app := internal.NewApp(os.Stdout, os.Args)
+	app.Run(ctx)
 }
